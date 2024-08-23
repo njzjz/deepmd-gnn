4cba30b3c449a5ed9c2350f73e62f980821788ac
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-08-22 21:25:49 -0400

    fix
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- noxfile.py
+++ noxfile.py
@@ -8,7 +8,7 @@ import nox
 @nox.session
 def tests(session: nox.Session) -> None:
     """Run test suite with pytest."""
-    session.install("torch", "-i", "https://download.pytorch.org/whl/cpu")
+    session.install("numpy", "torch", "-i", "https://download.pytorch.org/whl/cpu")
     cmake_prefix_path = session.run(
         "python",
         "-c",
