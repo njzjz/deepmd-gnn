7cbc24151dbb648e687544dfa3a2048df8aa58dc
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-08-22 23:08:52 -0400

    tests: check examples (#4)
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

---
+++ tests/test_examples.py
@@ -0,0 +1,28 @@
+"""Test examples."""
+
+import json
+from pathlib import Path
+
+import pytest
+from dargs.check import check
+from deepmd.utils.argcheck import gen_args
+
+from deepmd_mace.argcheck import mace_model_args  # noqa: F401
+
+example_path = Path(__file__).parent.parent / "examples"
+
+examples = (
+    example_path / "water" / "mace" / "input.json",
+    example_path / "dprc" / "mace" / "input.json",
+)
+
+
+@pytest.mark.parametrize("example", examples)
+def test_examples(example: Path) -> None:
+    """Check whether examples meet arguments."""
+    with example.open("r") as f:
+        data = json.load(f)
+    check(
+        gen_args(),
+        data,
+    )
