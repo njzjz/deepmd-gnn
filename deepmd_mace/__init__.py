2c34fdca80769e3514da9ab77ac46de2afdb43ce
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-08-22 21:57:20 -0400

    prevent partially initialized module
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- deepmd_mace/__init__.py
+++ deepmd_mace/__init__.py
@@ -2,12 +2,10 @@
 
 from ._version import __version__
 from .argcheck import mace_model_args
-from .mace import MaceModel as MACE  # noqa: N814
 
 __email__ = "jinzhe.zeng@rutgers.edu"
 
 __all__ = [
     "__version__",
-    "MACE",
     "mace_model_args",
 ]
